a23ddaa144b65d6a7a88d27702cad058dc6e0e31
DevCatalyst-2025 <devcatalyst.2025@gmail.com>
2025-08-28 18:34:32 +0530

    Updated usernames

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.set_page_config(
 # CONSTANTS & CONFIGURATION
 # -----------------------------
 ROLE_USERNAMES = {
-    "Representatives": ["RajLikhit", "karthikkoduri", "eshasatvase"],
+    "Representatives": ["RajLikhit", "karthikkoduri", "eshasatvase", "nbavana1"],
     "Members": ["mem1", "mem2"],
     "Admin": ["admin"]
 }
